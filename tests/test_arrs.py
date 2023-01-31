36cd45be0ce3cdfd58cd946aad60992adf052634
rusov63 <rusov63@yahoo.com>
2023-01-31 23:17:29 +0400

    четвертая задача тест прошел

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,7 +7,10 @@ class TestArrs(unittest.TestCase):
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
         self.assertEqual(arrs.get(['test'], 0, "test"), "test")
-
+        self.assertEqual(arrs.get([1, 2], -2, "test"), "test")
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([], 1, 2), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -6, 1), [1])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -2), [2, 3])
